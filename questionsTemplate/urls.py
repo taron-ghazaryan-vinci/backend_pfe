24d7038797d126c7532c8b5ebd0935438563490f
wissal-bouayyad-vinci <wissal.bouayyad@student.vinci.be>
2024-12-05 20:35:03 +0100

    add : views

--- questionsTemplate/urls.py
+++ questionsTemplate/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import CreateQuestionView, DeleteQuestionView, GetQuestionByIdView, GetAllQuestionsView, UpdateQuestionView
 
 urlpatterns = [
-    path('create/', CreateQuestionView.as_view(), name='create_question'),
-    path('delete/<str:question_id>/', DeleteQuestionView.as_view(), name='delete_question'),
-    path('get/<str:question_id>/', GetQuestionByIdView.as_view(), name='get_question_by_id'),
-    path('all/', GetAllQuestionsView.as_view(), name='get_all_questions'),
-    path('update/<str:question_id>/', UpdateQuestionView.as_view(), name='update_question'),
+    path('createQuestionTemplate/', CreateQuestionView.as_view(), name='create_question'),
+    path('deleteOneQuestionTemplate/<str:question_id>/', DeleteQuestionView.as_view(), name='delete_question'),
+    path('getOneQuestionTemplate/<str:question_id>/', GetQuestionByIdView.as_view(), name='get_question_by_id'),
+    path('getAllQuestionsTemplate/', GetAllQuestionsView.as_view(), name='get_all_questions'),
+    path('updateOneQuestionTemplate/<str:question_id>/', UpdateQuestionView.as_view(), name='update_question'),
 ]
--- questionsTemplate/views.py
+++ questionsTemplate/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 class CreateQuestionView(APIView):
+    def post(self,request):
+
 class DeleteQuestionView(APIView):
+
 class GetQuestionByIdView(APIView):
 class GetAllQuestionsView(APIView):
 class UpdateQuestionView(APIView):
