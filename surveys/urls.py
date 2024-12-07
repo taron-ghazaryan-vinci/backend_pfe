b9c808aae8a44b4d47c23c5d4a61bdcbfe48861a
wissal-bouayyad-vinci <wissal.bouayyad@student.vinci.be>
2024-12-07 12:29:57 +0100

    correction : import

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from surveys.views import createSurveyView
+from surveys.views import CreateSurveyView
 
 urlpatterns = [
     path('create-survey/<str:company_email>/', CreateSurveyView.as_view(), name='create_survey'),
