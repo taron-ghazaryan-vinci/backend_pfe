bb2150141f096afc78b2a3ddb75c7857cac815c1
steven-eklou-vinci <steven.eklou@student.vinci.be>
2024-12-09 10:41:02 +0100

    refactor : refactoring url

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from .views import (RegisterView, LoginView, SetTemplateTrueView,
 
 urlpatterns = [
     path('', GetAllUsersView.as_view(), name='get_all_users'),
-    path('<str:user_id>/', GetUserByIdView.as_view(), name='get_user_by_id'),
+    path('users/<str:user_id>/', GetUserByIdView.as_view(), name='get_user_by_id'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('set-template-true/<str:custom_id>/', SetTemplateTrueView.as_view(), name='set_template_true'),
