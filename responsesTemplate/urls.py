dce2bd3579a7eef1c0ef4e757b0b1378f6887e6d
wissal-bouayyad-vinci <wissal.bouayyad@student.vinci.be>
2024-12-10 11:38:09 +0100

    refactor : urlpatterns

--- responsesTemplate/urls.py
+++ responsesTemplate/urls.py
@@ -5,6 +5,6 @@ from .views import ClientResponseView, GetClientResponsesView, SubmitClientRespo
 urlpatterns = [
     path('responses/submit/', SubmitClientResponseView.as_view(), name='submit_response'),
     path('responses/<str:company_id>/', GetClientResponsesView.as_view(), name='get_client_responses'),  
-    path('response/<int:company_id>/<int:question_id>/', ClientResponseView.as_view(), name='client_response'),
+    path('response/<str:company_id>/<str:question_id>/', ClientResponseView.as_view(), name='client_response'),
 
 ]
