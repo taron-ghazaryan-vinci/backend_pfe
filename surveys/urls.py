8157c489ad08cd48feac2313c300ca8573efb768
axmad <mostafa.axmadmuxamad@student.vinci.be>
2024-12-11 03:03:27 +0100

    score

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from surveys.views import CreateSurveyView, GetEngagementsClientView
+from surveys.views import CreateSurveyView, GetEngagementsClientView, SubmitListQuestionsView, GlobalScoreView
+
 
 urlpatterns = [
     path('create-survey/<str:company_email>/', CreateSurveyView.as_view(), name='create_survey'),
     path('engagements/<str:company_email>/', GetEngagementsClientView.as_view(), name='get_engagements'),
+    path('submit-questions/<str:company_email>/', SubmitListQuestionsView.as_view(), name='submit_questions'),
+    path('calculate-global-score/<str:company_email>/', GlobalScoreView.as_view(), name='calculate_global_score')
 ]
